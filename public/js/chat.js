1159641d5b60c4f48fc525a2b79ff03b9d9b37ee
Salman Bashar <salman35-1493@diu.edu.bd>
2019-04-14 09:39:52 +0600

    Version 1.5
    -- Added User Interface
    -- Added timestamp for messages
    -- Storing user data
    -- Added user tracking
    -- Added automatic scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,14 +9,69 @@ const $messages = document.querySelector('#messages')
 
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+//Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+//Feature: Autoscroll
+const autoscroll = () => {
+  //New Message Element
+  const $newMessage = $messages.lastElementChild
+
+  //Height of the New Message
+  const newMessageStyles = getComputedStyle($newMessage)
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+  //Visible height
+  const visibleHeight = $messages.offsetHeight
+
+  //Height of Messages Container
+  const containerHeight = $messages.scrollHeight
+
+  //How Far have I Scrolled?
+  const scrollOfset = $messages.scrollTop + visibleHeight
+
+  if (containerHeight - newMessageHeight <= scrollOfset) {
+    $messages.scrollTop = $messages.scrollHeight
+  }
+}
 
 socket.on('message', (message) => {
   console.log(message)
   const html = Mustache.render(messageTemplate, {
-    message
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')  //Timestamp of posting a message
   })
   $messages.insertAdjacentHTML('beforeend', html)
+  autoscroll()
 })
+
+//Optional Feature: Geolocation
+
+socket.on('locationMessage', (message) => {
+  console.log(url)
+  const html = Mustache.render(locationMessageTemplate, {
+    usrnamer: message.username,
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  })
+  $message.innerAdjacentHTML('beforeend', html)
+  autoscroll()
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
+//Optional Feature: Geolocation
 
 $messageForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -59,4 +114,11 @@ $sendLocationButton.addEventListener('click', () => {
   })
 })
 
-//Optional Feature: Sharing GEO location
+
+//Feature: Joining Room
+socket.emit('join', { username,room }, (error) => {
+  if (error) {
+    alert(error)
+    location.href = '/'
+  }
+} )
--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app) //Refactoring?
 const io = socketio(server)
 
-const port = 3000
+const port = 8080
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
@@ -21,11 +23,34 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
   console.log('New WebSocket Connected')
 
-  socket.emit('message', 'Welcome!') //This emits only to the new user
+  socket.on('join', ({ username,room }, callback) => {
 
-  socket.broadcast.emit('message', 'A new user has joined!') //This emits to everyone except the the particular socket
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if (error) {
+      return callback(error)
+    }
+
+    //Join a specific room
+    socket.join(user.room)
+
+    //This emits a message only to the new use
+    socket.emit('message', generateMessage('System', 'Welcome! ' +user.username))
+
+    //This emits to everyone in the room except the the particular socket
+    socket.broadcast.to(user.room).emit('message', generateMessage('System', user.username + ' has joined!'))
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    })
+
+    callback()
+  })
 
   socket.on('sendMessage', (message, callback) => {
+
+    const user = getUser(socket.id)
 
     //Optional Feature: Profanity Filter
     const filter = new Filter()
@@ -35,18 +60,29 @@ io.on('connection', (socket) => {
     }
     //Optional Feature: Profanity Filter
 
-    io.emit('message', message) //This emits to everyone
+    io.to(user.room).emit('message', generateMessage(user.username, message)) //This emits messages to everyone
     callback()
   })
 
   //Optional Feature: Sharing GEO location
   socket.on('sendLocation', (coords) => {
-    io.emit('message', 'https://google.com/maps?q='+coords.latitude+','+coords.longitude)
+    const user = getUser(socket.id)
+    io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, 'https://google.com/maps?q='+coords.latitude+','+coords.longitude))
   })
   //Optional Feature: Sharing GEO location
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!')
+
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', generateMessage('System', user.username +' has left the chat room!'))
+
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
+    }
   })
 })
 
